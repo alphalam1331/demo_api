0887d4519fcd441331fd7f31e2d90defd3e40b3d
alphalam1331 <110608781+alphalam1331@users.noreply.github.com>
2024-03-09 17:38:44 +0800

    fix script (#17)

--- scripts/rolling-update.sh
+++ scripts/rolling-update.sh
@@ -1,14 +1,14 @@
 #! bash
 
 # path
-WORK_DIR="$HOME"/demo_app
+WORK_DIR="$HOME"/demo_api
 PNPM="$HOME"/.local/share/pnpm/pnpm
 
 cd "$WORK_DIR"
 
 PREVIOUS_CONTAINER_ID=$("$PNPM" run --silent prod:docker ps --services nest-app -q)
 
-"$PNPM" prod:docker build
+"$PNPM" prod:docker build nest-app
 
 docker container kill -s SIGTERM "$PREVIOUS_CONTAINER_ID"
 docker container remove -f "$PREVIOUS_CONTAINER_ID"
