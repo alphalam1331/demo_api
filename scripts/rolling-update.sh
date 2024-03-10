af813c99c9d3c8bbb2a3e2d3b5998e24f7cc96f0
alphalam1331 <110608781+alphalam1331@users.noreply.github.com>
2024-03-10 23:08:28 +0800

    update compose file, cd and script (#19)

--- scripts/rolling-update.sh
+++ scripts/rolling-update.sh
@@ -6,13 +6,6 @@ PNPM="$HOME"/.local/share/pnpm/pnpm
 
 cd "$WORK_DIR"
 
-PREVIOUS_CONTAINER_ID=$("$PNPM" run --silent prod:docker ps --services nest-app -q)
-
 "$PNPM" prod:docker build nest-app
-
-if [ -n $PREVIOUS_CONTAINER_ID ]; then
-    docker container kill -s SIGTERM "$PREVIOUS_CONTAINER_ID"
-    docker container remove -f "$PREVIOUS_CONTAINER_ID"
-fi
 
 "$PNPM" prod:docker up -d --no-deps nest-app
